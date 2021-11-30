39899119d9da9867a0a8c6403c89d602b0d1c65d
tsuhao <1355738338@qq.com>
2021-11-30 22:48:05 +0800

    add oh-my-zsh sh

---
+++ omz/zsh.sh
@@ -0,0 +1,7 @@
+
+echo "------- Installing oh-my-zsh && init .zshrc -------"
+wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.#!/bin/sh
+bash install.sh
+cp ./.zshrc ~/.zshrc
+
+echo "------- Installed oh-my-zsh -------"
